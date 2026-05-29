67e6e8cd2e7eb3a9a35e8e29316a01bbca55d8bf
Mrkrishan24 <krishanvarshney01818@gmail.com>
2026-05-29 16:43:10 +0530

    contact us changes

--- src/modules/website/pages/Contact/ContactUs.js
+++ src/modules/website/pages/Contact/ContactUs.js
@@ -89,7 +89,7 @@ const ContactUs = () => {
                             </div>
                             <div>
                                 <h4 style={{ fontWeight: 'bold', marginBottom: '5px' }}>Phone Number</h4>
-                                <p style={{ color: '#666', fontSize: '14px' }}>+1 (555) 123-4567</p>
+                                <p style={{ color: '#666', fontSize: '14px' }}>+91 9216621756</p>
                             </div>
                         </div>
 
@@ -190,7 +190,7 @@ const ContactUs = () => {
             {/* Map */}
             <div style={{ width: '100%', height: '450px', marginTop: '60px' }}>
                 <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.1422937950147!2d-73.9873196845941!3d40.75889497932681!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25855c6480299%3A0x55194ec5a1ae072e!2sTimes%20Square!5e0!3m2!1sen!2sus!4v1626359048384!5m2!1sen!2sus"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3528.0107181708704!2d77.31311769999999!3d28.3971956!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cdd2e7679ce6b%3A0x36927f402b606532!2s%26work%20Co-working!5e1!3m2!1sen!2sin!4v1780053047867!5m2!1sen!2sin"
                     width="100%"
                     height="100%"
                     style={{ border: 0, filter: 'grayscale(100%)' }}
